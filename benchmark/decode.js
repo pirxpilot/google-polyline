6000399785517e7a3040eea9415c2c8a2a4979e2
Damian Krzeminski <pirxpilot@code42day.com>
2016-12-17 20:23:48 -0800

    compare benchmarks with leaflet implementation

--- benchmark/decode.js
+++ benchmark/decode.js
@@ -1,4 +1,5 @@
 var polyline = require( '..' )
+var leaflet = require('polyline-encoded')
 var example = require( '../test/data/example-01' )
 
 suite( 'decode', function() {
@@ -9,6 +10,18 @@ suite( 'decode', function() {
 
   bench( '~350 points', function() {
     return polyline.decode( example.polyline )
+  })
+
+})
+
+suite('leaflet decode', function() {
+
+  bench( '3 points', function() {
+    return leaflet.decode( '_p~iF~ps|U_ulLnnqC_mqNvxq`@' )
+  })
+
+  bench( '~350 points', function() {
+    return leaflet.decode( example.polyline )
   })
 
 })
--- benchmark/encode.js
+++ benchmark/encode.js
@@ -1,4 +1,5 @@
 var polyline = require( '..' )
+var leaflet = require('polyline-encoded')
 var example = require( '../test/data/example-01' )
 
 suite( 'encode', function() {
@@ -13,6 +14,22 @@ suite( 'encode', function() {
 
   bench( '~350 points', function() {
     return polyline.encode( example.points )
+  })
+
+})
+
+suite( 'leaflet encode', function() {
+
+  bench( '3 points', function() {
+    return leaflet.encode([
+      [ 38.5, -120.2 ],
+      [ 40.7, -120.95 ],
+      [ 43.252, -126.453 ]
+    ])
+  })
+
+  bench( '~350 points', function() {
+    return leaflet.encode( example.points )
   })
 
 })
